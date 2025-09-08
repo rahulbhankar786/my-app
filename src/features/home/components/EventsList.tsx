c46e30b7371be64a9eb3b292ce5654d2dcf08ede
rahulkumar <rahulbhankar786@gmail.com>
2025-09-08 10:20:30 +0530

    improve list

--- src/features/home/components/EventsList.tsx
+++ src/features/home/components/EventsList.tsx
@@ -1,14 +1,18 @@
 import { useEvents } from "@myapp/hooks/useEvents";
-import { FlatList } from "react-native";
+import { FlatList, Text } from "react-native";
 import EventItem from "./EventItem";
 import { EventItem as EventItemType } from "types";
 import Placeholder from "./Placeholder";
+import { useCallback } from "react";
 
 const EventsList = () => {
     const {
     events, nextPage, loading
     } = useEvents();
-    
+
+    const renderItem = useCallback(({ item, index }: { item: EventItemType; index: number; }) => 
+        <EventItem event={item} index={index}/>,
+    [])
     return (
         <FlatList
             style={{ width: '90%' }}
@@ -21,7 +25,7 @@ const EventsList = () => {
             }}
             onEndReachedThreshold={0.9}
             data={events}
-            renderItem={({ item, index }: { item: EventItemType; index: number; }) => <EventItem event={item} index={index}/>}
+            renderItem={renderItem}
             keyExtractor={(item) => item.id}
         />
     );
--- src/features/home/components/Placeholder.tsx
+++ src/features/home/components/Placeholder.tsx
@@ -2,19 +2,21 @@ import React from "react";
 import { View } from "react-native";
 import { SCREEN_HEIGHT } from "@myapp/utils/utils";
 
-const Placeholder = () => 
-  Array.from({ length: 5 }).map((_, index) => (
-    <React.Fragment key={`placeholder-${index}`}>
+const Placeholder = () => {
+  return [1,2,3,4,5].map((_, index) => (
       <View
+        key={`placeholder-${index}`}
         style={{
           height: SCREEN_HEIGHT * 0.25,
+          width: '90%',
           backgroundColor: '#e0e0e0',
           borderRadius: 16,
           marginVertical: 10,
           alignSelf: 'center',
         }}
       />
-    </React.Fragment>
   ))
+}
+  
 
 export default Placeholder;
